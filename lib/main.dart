a7662069e86f71034e9dd6d2380f8580b2b03ed6
xuwentao93 <a15157756529@gmail.com>
2020-02-06 22:19:42 +0800

    finished primary layout tutorial

--- lib/main.dart
+++ lib/main.dart
@@ -9,16 +9,36 @@ class MyApp extends StatelessWidget {
   MyApp({Key key, @required this.items}): super(key: key);
   @override
   Widget build(BuildContext context) {
+    var stack = Stack(
+      children: <Widget>[
+        CircleAvatar(
+          backgroundImage: NetworkImage('http://newimg.jspang.com/zhishixingqiu.png'),
+          radius: 100.0
+        ),
+        Positioned(
+          top: 100.0,
+          left: 10.0,
+          child: Container(
+            child: Text('hello world'),
+            decoration: BoxDecoration(
+              color: Color.fromRGBO(255, 0, 0, 1)
+            ),
+            padding: EdgeInsets.all(10.0)
+          )
+        )
+      ],
+    );
     return MaterialApp(
       title: 'this is a primary flutter demo',
       home: Scaffold(
         appBar: AppBar(
-          title: Text('welcome to flutter writen by xuwentao')
+          title: Text('welcome to flutter demo')
         ),
         body: ListView(
           children: <Widget>[
             ListTile(
               leading: Icon(Icons.perm_camera_mic),
+              subtitle: Text('a brief text'),
               title: Text('this is a text introduce speech sounds')
             ),
             ListTile(
@@ -76,6 +96,38 @@ class MyApp extends StatelessWidget {
                     )
                   )
                 ],
+              )
+            ),
+            Container(
+              child: Column(
+                children: <Widget>[
+                  Text('hello world'),
+                  Text('hi, this is a second line')
+                ],
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                mainAxisAlignment: MainAxisAlignment.end,
+              )
+            ),
+            Container(
+              child: stack
+            ),
+            Container(
+              child: Card(
+                child: Column(
+                  children: <Widget>[
+                    ListTile(
+                      title: Text('hello, this is a first text'),
+                      subtitle: Text('hello, this is a brief text'),
+                      leading: Icon(Icons.perm_camera_mic)
+                    ),
+                    Divider(),
+                    ListTile(
+                      title: Text('hello world'),
+                      subtitle: Text('hello, small world'),
+                      leading: Icon(Icons.perm_identity)
+                    )
+                  ],
+                )
               )
             )
           ],
